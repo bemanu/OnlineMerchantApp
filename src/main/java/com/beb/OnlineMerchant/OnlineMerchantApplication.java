2d4c294bce60ef0b476e54c2fc8617ebaae8ff99
Manu <bemanub@gmail.com>
2021-12-29 12:07:44 +0000

    Online Merchant application

---
+++ src/main/java/com/beb/OnlineMerchant/OnlineMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.beb.OnlineMerchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMerchantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMerchantApplication.class, args);
+	}
+
+}
